7fd2e5a3b0a8bf3ccdf65f56726609b7d8cca7e7
Alvin Williams <alvin.williams@goemerchant.com>
2018-03-15 20:51:12 -0400

    Added .gitignore and changed some of the code to detect an old 3D Cart Store and new 3D Cart Store.

--- 3DCartAutoClicker.py
+++ 3DCartAutoClicker.py
@@ -1,38 +1,45 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-options = webdriver.ChromeOptions()
-prefs = {"profile.default_content_setting_values.notifications" : 2}
-options.add_experimental_option('prefs', prefs)
-driver = webdriver.Chrome('/chromedriver', options=options) #locate the webdriver in your file directory
-username = "" #enter your FB email
-password = "" #enter your FB password
-
-def openTinder():
-    driver.get("https://tinder.com/app/login")
-    driver.implicitly_wait(10)
-    driver.find_element_by_xpath("//*[@id='modal-manager']/div/div/div[2]/div[1]/div[2]/button[1]").click()
-    
-def fbLogin():
-    driver.find_element_by_xpath("//*[@id='email']").send_keys(username)
-    driver.find_element_by_xpath("//*[@id='pass']").send_keys(password)
-    driver.find_element_by_xpath("//*[@id='u_0_0']").click()
-
-
-def like():
-    like = driver.find_element_by_xpath("//*[@id='content']/div/span/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[4]")
-    while True:
-        try:
-            like.click()
-        except NoSuchElementException:
-            print("hello")
+class AutoClicker:
+    def __init__(self, url, transId, gatewayId, processorId):
+        self.url = url
+        self.transId = transId
+        self.gatewayId = gatewayId
+        self.processorId = processorId
+        self.options = webdriver.ChromeOptions()
+        self.prefs = {"profile.default_content_setting_values.notifications" : 2}
+        self.options.add_experimental_option('prefs', self.prefs)
+        self.driver = webdriver.Chrome('chromedriver', options=self.options)#locate the webdriver in your file directory
+        self.username = "" 
+        self.password = "" 
+        
+    def open3DCart(self):
+        self.driver.get(self.url + "gem.asp?action=createSupportUser")
+        self.driver.implicitly_wait(4)
+        self.driver.find_element_by_partial_link_text("Login").click()
+        className = "dropdown-toggle"
+        dropdownExist = self.checkExistByClass(className)
+        if(dropdownExist):
+            self.new3DCartStore()
+        else:
+            self.old3DCartStore()
             
-
+    def old3DCartStore(self):
+        self.driver.get(self.url + "payment_options.asp?paymethod=online_payment")
+        
+    def new3DCartStore(self):
+        self.driver.get(self.url + "payment_options.asp?paymethod=online_payment")
+        
+    def checkExistByClass(self, className):
+        try:
+            self.driver.find_element_by_class_name(className)
+        except NoSuchElementException:
+            return False
+        return True
+    
 if __name__ == "__main__":
-    openTinder()
-    driver.switch_to_window(driver.window_handles[-1])
-    fbLogin()
-    driver.switch_to_window(driver.window_handles[0])
-    like()
+    a = AutoClicker("https://integrityinstuments-com.3dcartstores.com/admin/", 1, 2, 3)
+    a.open3DCart()
     
 
